744bee417ebe2a8431828f20fa3876432dbe0de1
steve51516 <steve51516@gmail.com>
2021-03-09 18:13:43 -0500

    corrected callsign mapping

--- db.py
+++ db.py
@@ -36,7 +36,7 @@ def read_save(data):
       print("Successfully Connected to SQLite")
       sqlite_insert_query = """INSERT INTO weather(SampleDateTime, StationID, TemperatureF, Pressure, Humidity, pm25, pm10) 
       VALUES(?, ?, ?, ?, ?, ?, ?);"""
-      data_tuple = (data['now'], data['call'], data['temperature'], data['pressure'], data['humidity'], data['pm25'], data['pm10'])
+      data_tuple = (data['now'], data['callsign'], data['temperature'], data['pressure'], data['humidity'], data['pm25'], data['pm10'])
       cursor.execute(sqlite_insert_query, data_tuple)
       sqliteConnection.commit()
       print("Record inserted successfully into weather table ", cursor.rowcount)
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ if __name__=="__main__":
     sensor = Sensor(0x77)
     db.make_table()
     data = {}
+    data['callsign'] = config['aprs']['callsign']
     for item in config['sensors']:
         if config['sensors'].getboolean(item) is False:
             data[item] = "..."
@@ -19,7 +20,6 @@ if __name__=="__main__":
         data['humidity'] = int(tmp['humidity'])
         data['temperature'] = int(sensor.get_temperature(unit='F'))
         data['now'] = time.strftime('%H%M%S', time.gmtime())
-        data['call'] = 'NOCALL'
         pm25,pm10 = read_sds011(config)
         data['pm25'] = pm25
         data['pm10'] = pm10
