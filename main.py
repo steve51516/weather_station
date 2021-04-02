78efc5279a40631d0b8ea916084599ca6cc7026d
steve51516 <steve51516@gmail.com>
2021-04-02 19:27:47 +0000

    optimized code

--- main.py
+++ main.py
@@ -9,29 +9,27 @@ if __name__=="__main__":
     #sensor = Sensor(hex(config['bme280']['device']))
     sensor = Sensor(0x77)
     db.make_table()
-    data = {}
-    data['callsign'] = config['aprs']['callsign']
+    data = { 'callsign': config['aprs']['callsign'] }
     for item in config['sensors']: # If an item in config is boolean false assign value of "..."
         if config['sensors'].getboolean(item) is False: data[item] = "..."
     while True:
         data += sensor.get_data()
         data['temperature'] = int(sensor.get_temperature(unit='F'))
+
         if config['serial'].getboolean('enabled') is True: # If SDS011 is enabled collect readings
             pm25,pm10 = read_sds011(config)
-            data['pm25'] = pm25
-            data['pm10'] = pm10
+            data['pm25'], data['pm10'] = pm25, pm10
         else:
-            data['pm25'] = 0
-            data['pm10'] = 0
+            data['pm25'], data['pm10'] = 0, 0
+
         if config.getboolean('aprs', 'sendall'):
-            data['packet'] = aprs.send_data(data, config, sendall=True)
-            data['sent'] = 1
+            data['sent'], data['packet'] = 1, aprs.send_data(data, config, sendall=True)
         else:
-            data['packet'] = aprs.send_data(data, config)
-            data['sent'] = 0
+            data['sent'], data['packet'] = 0, aprs.send_data(data, config)
+
         if config['sensors'].getboolean('quiet') is False:
             print(data['packet'])
             show_air_values(config)
+
         db.read_save_enviro(data); db.read_save_packet(data) # Write to database
-        stdout.flush()
-        time.sleep(300) # Wait 5 minutes
+        stdout.flush(); time.sleep(300) # Wait 5 minutes
