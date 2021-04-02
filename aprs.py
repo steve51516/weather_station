1a62f6c8fecbd9eeabc73bc51514d28fded69b15
steve51516 <steve51516@gmail.com>
2021-04-02 18:57:18 +0000

    implimented multiple server options in config

--- aprs.py
+++ aprs.py
@@ -1,18 +1,7 @@
 import aprslib
 from bme280pi import Sensor
 
-# The first four fields (wind direction, wind speed, temperature and gust) are required,
-# in that order, and if a particular measurement is not present, 
-# the three numbers should be replaced by "..." to indicate no data available.
 def send_data(data, config, sendall=False):
-    # data['longitude'] = ""
-    # data['latitude'] = ""
-    # data['wdir'] = "..." # 3 numbers represents wind direction in degrees from true north. This is the direction that the wind is blowing from.
-    # data['avgwind'] = "..." # 3 numbers represents Average wind speed in MPH
-    # data['peakwind'] = "..." # 3 numbers represents Peak wind speed
-    # data['temperature'] =  # The letter "t" followed by 3 characters (numbers and minus sign) represents the temperature in degrees F.
-    # data['rain1h'] = "..." # The letter "r" followed by 3 numbers represents the amount of rain in hundredths of inches that fell the past hour.
-    # data['rain24h'] = "..." # The letter "p" followed by 3 numbers represents the amount of rain in hundredths of inches that fell in the past 24 hours.
 
     if data['temperature'] < 100 and data['temperature'] > 9: # Add 0 in front if temperature is between 0 and 99
         data['temperature'] = f"0{data['temperature']}"
@@ -26,13 +15,14 @@ def send_data(data, config, sendall=False):
     data['pressure'] = round(data['pressure'], 1) # The letter "b" followed by 5 numbers represents the barometric pressure in tenths of a millibar.
     if data['humidity'] == 100:
         data['humidity'] = "00"
-
-    #   data['humidity'] = int(data['humidity'])
+    
     packet = f"{config['aprs']['callsign']}>APRS,TCPIP*:@{data['ztime']}z{config['aprs']['longitude']}/{config['aprs']['latitude']}_{data['wdir']}/{data['avgwind']}g{data['peakwind']}t{data['temperature']}r{data['rain1h']}p{data['rain24h']}P{data['rain00m']}b{data['pressure']}h{data['humidity']}{config['aprs']['comment']}"
+
     if sendall:
-        AIS = aprslib.IS(config['aprs']['callsign'], config['aprs']['passwd'], config['aprs']['server_pool'], config['aprs']['port'])
-        AIS.connect()
-        AIS.sendall(packet)
-        AIS.close()
+        for server in config['servers']:
+            AIS = aprslib.IS(config['aprs']['callsign'], config['aprs']['passwd'], config['servers'][server], config['aprs']['port'])
+            AIS.connect()
+            AIS.sendall(packet)
+            AIS.close()
 
     return packet
