676e9234cb51b87c0133c03808bd6c2258d2c9c4
steve51516 <steve51516@gmail.com>
2021-04-05 14:38:07 +0000

    move packet to format_data

--- aprs.py
+++ aprs.py
@@ -2,7 +2,7 @@ import aprslib, time
 from bme280pi import Sensor
 from math import trunc
 
-def format_data(data):
+def format_data(data, config):
         data['pressure'] = trunc(round(data['pressure'], 2) * 10.) # shift decimal point to the left 1 and round
         data['humidity'] = int(data['humidity'])
         data['ztime'] = time.strftime('%d%H%M', time.gmtime()) # Get zulu/UTC time
@@ -18,12 +18,13 @@ def format_data(data):
         # Humidity must be 2 digits. If humidity is 100% assign value of 00
         if data['humidity'] == 100:
             data['humidity'] = "00"
-        
-        return data
+
+        packet = f"{config['aprs']['callsign']}>APRS,TCPIP*:@{data['ztime']}z{config['aprs']['longitude']}/{config['aprs']['latitude']}_{data['wdir']}/{data['avgwind']}g{data['peakwind']}t{data['temperature']}r{data['rain1h']}p{data['rain24h']}P{data['rain00m']}b{data['pressure']}h{data['humidity']}{config['aprs']['comment']}"
+
+        return packet
         
 def send_data(data, config, sendall=False):
-    data = format_data(data)
-    packet = f"{config['aprs']['callsign']}>APRS,TCPIP*:@{data['ztime']}z{config['aprs']['longitude']}/{config['aprs']['latitude']}_{data['wdir']}/{data['avgwind']}g{data['peakwind']}t{data['temperature']}r{data['rain1h']}p{data['rain24h']}P{data['rain00m']}b{data['pressure']}h{data['humidity']}{config['aprs']['comment']}"
+    packet = format_data(data, config)
     if sendall:
         for server in config['servers']:
             AIS = aprslib.IS(config['aprs']['callsign'], config['aprs']['passwd'], config['servers'][server], config['aprs']['port'])
