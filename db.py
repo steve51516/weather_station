d5759787c2aaefe0cc8875011e7160bd8730da86
steve51516 <steve51516@gmail.com>
2021-04-07 19:14:16 +0000

    remove db creation functions

--- db.py
+++ db.py
@@ -1,46 +1,6 @@
 import mariadb as db
 from time import sleep
 
-def create_db():
-    db_init = """ CREATE DATABASE IF NOT EXISTS weather;"""
-    weather_table_init = """CREATE TABLE IF NOT EXISTS sensors (
-                ID BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                stationid VARCHAR(10),
-                created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                ambient_temperature DECIMAL(6,2) NOT NULL,
-                wind_direction DECIMAL(6,2),
-                wind_speed DECIMAL(6,2),
-                wind_gust_speed DECIMAL(6,2),
-                humidity DECIMAL(6,2) NOT NULL,
-                rainfall DECIMAL(6,2),
-                air_pressure DECIMAL(6,2) NOT NULL,
-                PM25 DECIMAL(6,2),
-                PM10 DECIMAL(6,2)
-                );"""
-    packet_table_init = """CREATE TABLE IF NOT EXISTS packets(
-                           ID BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                           created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-                           packet VARCHAR(100) NOT NULL,
-                           transmitted BOOL NOT NULL
-                           );"""
-
-    try:
-        conn = db.connect(
-            user="wxstation",
-            password="password1",
-            host="127.0.0.1",
-            database="weather",
-            port=3306
-        )
-        cur = conn.cursor()
-        cur.execute(db_init); conn.commit()
-        cur.execute(weather_table_init); conn.commit()
-        cur.execute(packet_table_init); conn.commit()
-        conn.close()
-    except db.Error as e:
-        print(f"Error connecting to MariaDB Server: {e}")
-        #sys.exit(1)
-
 def db_connect():
     for i in range(1, 4): # Retry 3 times increasing delay by 10 seconds each time
         delay = i * 10
@@ -68,10 +28,10 @@ def read_save_sensors(data):
     cur.execute(sensors_insert, data_tuple)
     conn.commit(); conn.close()
 
-def read_save_packet(data):
+def read_save_packet(packet, transmitted):
     packet_insert = """INSERT INTO packets(packet, transmitted) 
     VALUES(?, ?);"""
-    data_tuple = (data['packet'], data['sent'])
+    data_tuple = (packet, transmitted)
     conn = db_connect()
     cur = conn.cursor()
     cur.execute(packet_insert, data_tuple)
