03d6d2f38814a8e81c207828284d2cde64953e69
steve51516 <steve51516@gmail.com>
2021-04-08 18:06:56 +0000

    added while loop to calculate_speed

--- wspeed.py
+++ wspeed.py
@@ -7,16 +7,17 @@ wind_count = 0
 wind_list = []
 
 def calculate_speed():
-    global wind_list; global wind_count
-    radius_cm = 9.0
-    cm_in_mile = 160934.4
-    circumference_cm = (2 * math.pi) * radius_cm
-    rotations = wind_count / 2.0
-    dist_miles = (circumference_cm * rotations) / cm_in_mile
-    speed = dist_miles / 5 # Divide distance by time, 5 seconds
-    wind_list.append(speed)
-    wind_count = 0 # Reset wind count
-    sleep(5)
+    while True:
+        global wind_list; global wind_count
+        radius_cm = 9.0
+        cm_in_mile = 160934.4
+        circumference_cm = (2 * math.pi) * radius_cm
+        rotations = wind_count / 2.0
+        dist_miles = (circumference_cm * rotations) / cm_in_mile
+        speed = dist_miles / 5 # Divide distance by time, 5 seconds
+        wind_list.append(speed)
+        wind_count = 0 # Reset wind count
+        sleep(5)
 
 def wind_avg(wlist):
     try:
