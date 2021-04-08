987cfe21141cb23f524130f8f20291b83c209057
steve51516 <steve51516@gmail.com>
2021-04-08 18:33:16 -0400

    set rainfall to 0 if disabled

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ if __name__=="__main__":
         print("Starting rainfall monitoring thread.")
         th_rain = th.Thread(target=monitor_rainfall, daemon=True)
         th_rain.start()
+    else:
+        data['rainfall'] = 0
     if config['sensors'].getboolean('wspeed'):
         from wspeed import monitor_wind, calculate_speed, wind_avg, wind_list
         print("Starting wind speed monitoring thread.")
@@ -38,19 +40,20 @@ if __name__=="__main__":
     print("Done reading config file.\nStarting main program now.")
 
     while True:
-        if 'th_sds011' in globals():
+        if 'th_sds011' in locals():
             th_sds011.start()
+        if th_rain in locals():
+            data['rainfall'] = tips; reset_rainfall() # 0 if disabled or actual value if enabled, reset after saving value
         data['temperature'] = sensor.get_temperature(unit='F')
         data['pressure'] = sensor.get_pressure()
         data['humidity'] = sensor.get_humidity()
-        data['rainfall'] = tips; reset_rainfall() # 0 if disabled or actual value if enabled, reset after saving value
 
-        if 'th_wmonitor' and 'th_wspeed' in globals():
+        if 'th_wmonitor' and 'th_wspeed' in locals():
             data['wspeed'] = wind_avg(wind_list)
             if len(wind_list) < 0:
                 data['wgusts'] = max(wind_list)
                 wind_list.clear()
-        if 'th_sds011' in globals():
+        if 'th_sds011' in locals():
             th_sds011.join()
             data['pm25'], data['pm10'] = air_values['pm25'], air_values['pm10']
 
