5b7a2ac6bdfba64dcf33309a1748f2c4a0160adb
steve51516 <steve51516@gmail.com>
2021-04-12 17:50:16 +0000

    dynamically create voltage to degrees dictionary

--- wdir.py
+++ wdir.py
@@ -1,30 +1,38 @@
-from gpiozero import MCP3008
+#from gpiozero import MCP3008
 
-adc = MCP3008(channel=0)
-count = 0
+#adc = MCP3008(channel=0)
+#count = 0
 values = []
-volts = {
-    0.4: 0.0,
-    1.4: 22.5,
-    1.2: 45.0,
-    2.8: 67.5,
-    2.7: 90.0,
-    2.9: 112.5,
-    2.2: 135.0,
-    2.5: 157.5,
-    1.8: 180.0,
-    2.0: 202.5,
-    0.7: 225.0,
-    0.8: 247.5,
-    0.1: 270.0,
-    0.3: 292.5,
-    0.2: 315.0,
-    0.6: 337.5
-}
+R1 = 10_000 # Static resistor value on board
+VIN = 3.3 # Input voltage can be 5.0 or 3.3
+resistances = [ # Resistor values inside wind vain
+    33_000,
+    6570,
+    8200,
+    891,
+    1000,
+    688,
+    2200,
+    1410,
+    3900,
+    3140,
+    16_000,
+    14_120,
+    120_000,
+    42_120,
+    64_900,
+    21_880
+]
 
-def wind_direction():
-    wind = round(adc.value * 3.3, 1)
-    if not wind in volts:
-        print(f"Unkown Value {wind} {volts[wind]}")
-    else:
-        print(f"found {wind} {volts[wind]}}")
+def voltage_divider(r1, r2, vin):
+    vout = (vin * r2) / (r1 + r2)
+    return round(vout, 3)
+
+def populate_V2D():
+    volts = {}
+    degrees = 0.0
+    for r2 in resistances:
+        voltage = voltage_divider(R1, r2, VIN)
+        volts[voltage] = degrees
+        degrees += 22.5
+    return volts
