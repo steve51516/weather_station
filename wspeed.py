e81fcb9c47ceae5ee2c645aa74d6d939172ce514
steve51516 <steve51516@gmail.com>
2021-04-15 14:28:51 +0000

    compensate for delayed speed calculations

--- wspeed.py
+++ wspeed.py
@@ -17,9 +17,11 @@ class WindMonitor:
 
     def calculate_speed(self, stop_event):
         delay = 5 # 5 seconds
+        extra_delay = 0 # If a stop event occurs, add to time needed to calculate wind speed
         while True:
             with self.wind_count_lock and self.wind_list_lock:
                 if self.wind_count > 0:
+                    delay += extra_delay
                     radius_cm = 9.0
                     cm_in_mile = 160934.4
                     circumference_cm = (2 * pi) * radius_cm
@@ -28,9 +30,11 @@ class WindMonitor:
                     miles_per_sec = dist_miles / delay # Divide distance by time
                     miles_per_hour = (miles_per_sec * 3600) * self.ADJUSTMENT # miles per second times seconds in an hour
                     self.wind_list.append(miles_per_hour) #; print(f"Wind speed of {miles_per_hour} recorded")
-                    self.wind_count = 0 #; print("wind_count has been reset!") # Reset wind count
+                    self.wind_count = 0 ; print("wind_count has been reset!") # Reset wind count
+                    extra_delay = 0 # Reset extra delay
                 sleep(delay)
             if stop_event.wait(5):
+                extra_delay += 5
                 continue
 
     def spin(self):
