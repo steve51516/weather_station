7ad0a25c298f714e2c9fd2b6d6323d5c87383018
steve51516 <steve51516@gmail.com>
2021-04-15 23:02:38 -0400

    restructure total_rain function

--- rainfall.py
+++ rainfall.py
@@ -2,12 +2,13 @@ from gpiozero import Button
 from threading import Lock
 from time import sleep
 
-DEFAULT_RAIN_SENSOR = Button(5)
-#BUCKET_SIZE = 0.2794 # mm
-BUCKET_SIZE = 0.011 # inches
+
 
 class RainMonitor:
-    def __init__(self, BUCKET=BUCKET_SIZE, button=DEFAULT_RAIN_SENSOR):
+    """ DEFAULT_RAIN_SENSOR = Button(5)
+        BUCKET_SIZE = 0.2794 # mm
+        BUCKET_SIZE = 0.011 # inches"""
+    def __init__(self, BUCKET=0.011, button=Button(5)):
         self.tips = 0
         self.button = button
         self.bucket_size = BUCKET_SIZE
@@ -19,17 +20,12 @@ class RainMonitor:
                 self.tips_lock.acquire()
                 self.tips += 1
                 #print(f"Bucket tipped! Total rainfall calculated is {self.tips * self.bucket_size}")
+                #TODO set above print statement as debug log level
                 self.tips_lock.release()
+                break
 
     def total_rain(self): # Convert tips to rain in hundreths of an inch and reset tips counter
-        while True:
-            if not self.tips_lock.locked():
-                self.tips_lock.acquire()
-                t_rain = self.tips * self.bucket_size
-                self.tips_lock.release()
-                return t_rain
-            else:
-                sleep(2)
+        return self.tips * self.bucket_size
 
     def clear_total_rain(self):
         while True:
