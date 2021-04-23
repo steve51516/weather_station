cdaf7419412ed3bc46852b350b68bb0073040dbd
steve51516 <steve51516@gmail.com>
2021-04-23 15:08:22 +0000

    removed thread locking

--- pysds011.py
+++ pysds011.py
@@ -1,4 +1,3 @@
-from threading import Lock
 from statistics import mean
 from time import sleep, time
 from sds011 import SDS011
@@ -14,31 +13,22 @@ class MonitorAirQuality:
             'pm10_errors': 0
         }
         self.air_values_lock = Lock()
-        self.interval = interval
+        self.interval = float(interval)
         self.sensor = SDS011(serial_port=tty, baudrate=baudrate)
 
     def read_sds011(self):
-        while True:
-            if not self.air_values_lock.locked():        
-                self.air_values_lock.acquire()
-
-                self.air_values['pm25'], self.air_values['pm10'] = self.sensor.query()
-
-                if self.air_values['pm25'] < 999.9:
-                    self.air_values['pm25_total'].append(self.air_values['pm25'])
-                else:
-                    self.air_values['pm10_errors'] += 1
-                    print(f"Error: PM25 value {self.air_values['pm25']} is out of range")
-                if self.air_values['pm10'] < 999.9:
-                    self.air_values['pm10_total'].append(self.air_values['pm10'])
-                else:
-                    self.air_values['pm10_errors'] += 1
-                    print(f"Error: PM10 value {self.air_values['pm10']} is out of range")
-                self.air_values_lock.release()
-                return self.air_values['pm25'], self.air_values['pm10']
-            else:
-                print("air_values is locked, retry average calculating in 2 seconds...")
-                sleep(2)
+        self.air_values['pm25'], self.air_values['pm10'] = self.sensor.query()
+        if self.air_values['pm25'] < 999.9:
+            self.air_values['pm25_total'].append(self.air_values['pm25'])
+        else:
+            self.air_values['pm10_errors'] += 1
+            print(f"Error: PM25 value {self.air_values['pm25']} is out of range")
+        if self.air_values['pm10'] < 999.9:
+            self.air_values['pm10_total'].append(self.air_values['pm10'])
+        else:
+            self.air_values['pm10_errors'] += 1
+            print(f"Error: PM10 value {self.air_values['pm10']} is out of range")
+        return self.air_values['pm25'], self.air_values['pm10']
 
     def show_air_values(self):
         #self.read_sds011()
@@ -46,18 +36,15 @@ class MonitorAirQuality:
         print("PM10, µg/m3: ", self.air_values['pm10'])
     
     def average(self): # Return average from total readings
-        while True:
-            if not self.air_values_lock.locked():
-                if len(self.air_values['pm25_total']) > 0 and len(self.air_values['pm10_total']) > 0:
-                    if len(self.air_values['pm25_total']) > 5 and len(self.air_values['pm10_total']) > 5: # delete first 5 readings
-                        self.air_values['pm25_total'] = self.air_values['pm25_total'][5:]
-                        self.air_values['pm10_total'] = self.air_values['pm10_total'][5:]
-                        return round(mean(self.air_values['pm25_total']), 2), round(mean(self.air_values['pm10_total']), 2)
-                    else:
-                        return round(mean(self.air_values['pm25_total']), 2), round(mean(self.air_values['pm10_total']), 2)
+        if len(self.air_values['pm25_total']) > 0 and len(self.air_values['pm10_total']) > 0:
+            if len(self.air_values['pm25_total']) > 5 and len(self.air_values['pm10_total']) > 5: # delete first 5 readings
+                self.air_values['pm25_total'] = self.air_values['pm25_total'][5:]
+                self.air_values['pm10_total'] = self.air_values['pm10_total'][5:]
+                return round(mean(self.air_values['pm25_total']), 2), round(mean(self.air_values['pm10_total']), 2)
             else:
-                print("air_values locked, retry average calculating in 2 seconds...")
-                sleep(2)
+                return round(mean(self.air_values['pm25_total']), 2), round(mean(self.air_values['pm10_total']), 2)
+        else:
+            print("pm25 and pm10 totals were empty, no readings to average.")
     
     def monitor(self):
         self.sensor.sleep(sleep=False)
