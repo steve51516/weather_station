37f4bf660beea2f14a33cdd3a2168cb9a91a36e5
steve51516 <steve51516@gmail.com>
2021-04-25 20:30:49 -0400

    change class name

--- si4713.py
+++ si4713.py
@@ -1,12 +1,12 @@
 import board, busio, adafruit_si4713, digitalio, os
 from playsound import playsound
 
-class si4713:
+class transmitter:
     def __init__(self, frequency=102300, rds=False, led=True, soundfile='/tmp/aprs_report.wav'):
         try:
             self.i2c = busio.I2C(board.SCL, board.SDA)
             self.si_reset = digitalio.DigitalInOut(board.D5)
-            self.si4713 = adafruit_si4713.SI4713(self.i2c, reset=self.si_reset, timeout_s=0.5)
+            self.transmitter = adafruit_si4713.SI4713(self.i2c, reset=self.si_reset, timeout_s=0.5)
         except Exception as e:
             print(f"Exception occured while initilization of si4713 transmitter:\n\t{e}")
 
@@ -14,25 +14,25 @@ class si4713:
         # Datasheet says you can only specify 50khz steps
         #self.FREQUENCY_KHZ = 102300  # 102.300mhz
         self.FREQUENCY_KHZ = frequency
-        self.si4713.tx_frequency_khz = self.FREQUENCY_KHZ
-        self.si4713.tx_power = 0 # Transmist power output range is 88 - 115 dBuV, 0 is off
+        self.transmitter.tx_frequency_khz = self.FREQUENCY_KHZ
+        self.transmitter.tx_power = 0 # Transmist power output range is 88 - 115 dBuV, 0 is off
         self.led = led # Not implimented yet
         self.soundfile = soundfile # Static sound file name/location. Background script will rotate sound files.
         if rds is True:
-            self.si4713.configure_rds(0xADAF, station=b"WxStation", rds_buffer=b"Some WxStation text")
+            self.transmitter.configure_rds(0xADAF, station=b"WxStation", rds_buffer=b"Some WxStation text")
 
     def get_freq_noise(self):
-        noise = self.si4713.received_noise_level(self.FREQUENCY_KHZ)
+        noise = self.transmitter.received_noise_level(self.FREQUENCY_KHZ)
         print(f"Noise at {0:0.3f} mhz: {1} dBuV".format(self.FREQUENCY_KHZ / 1000.0, noise))
     
     def get_audio_level(self):
-        print(f'Audio level: {0} dB'.format(self.si4713.input_level))
-        print(f'Audio signal status: 0x{0:02x}'.format(self.si4713.audio_signal_status))
+        print(f'Audio level: {0} dB'.format(self.transmitter.input_level))
+        print(f'Audio signal status: 0x{0:02x}'.format(self.transmitter.audio_signal_status))
     
     def scan_freq_noise(self):
         lowest_noise = 100 # Arbitrary number
         for f_khz in range(87500, 108000, 50):
-            noise = self.si4713.received_noise_level(f_khz)
+            noise = self.transmitter.received_noise_level(f_khz)
             if noise < lowest_noise:
                 lowest_noise = noise
                 best_freq = f_khz
@@ -53,9 +53,9 @@ class si4713:
         if os.path.isfile(self.soundfile):
             self.manage_soundfile(packet)
             self.tx_power = 115 # Turn on transmitter with max power
-            print("Transmitting at {0:0.3f} mhz".format(self.si4713.tx_frequency_khz / 1000.0))
-            print("Transmitter power: {0} dBuV".format(self.si4713.tx_power))
-            print("Transmitter antenna capacitance: {0:0.2} pF".format(self.si4713.tx_antenna_capacitance))
+            print("Transmitting at {0:0.3f} mhz".format(self.transmitter.tx_frequency_khz / 1000.0))
+            print("Transmitter power: {0} dBuV".format(self.transmitter.tx_power))
+            print("Transmitter antenna capacitance: {0:0.2} pF".format(self.transmitter.tx_antenna_capacitance))
             print("Starting transmission now...") 
             playsound(self.soundfile) # Play soundfile while transmitting
             self.tx_power = 0 # Turn off transmitter
